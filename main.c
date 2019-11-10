ce018eb891d09552d3ca826be54691ddf622ac75
dimitris <jimhasko@gmail.com>
2019-11-10 12:50:41 +0200

    second

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <inttypes.h>
 #include "quicjshort.h"
 #include <time.h>
-
+#include "shhh.h"
 
 
 
@@ -12,44 +12,88 @@
 
 
 int main() {
-    clock_t start = clock();
-char  filename[]="/home/dimitris/CLionProjects/jj2019/relA";
+
+char  filename1[]="/home/dimitris/CLionProjects/jj2019/relA";
+    char  filename2[]="/home/dimitris/CLionProjects/jj2019/relB";
 
 
 
-
-   int i,needed_column,j;
+   int i,needed_column,j,k,l;
    needed_column=2;
 
 
 
 
-Table_Info* table;
-table=get_table(filename,needed_column);
-results* not_yet;
-
-    not_yet=big_short(filename,needed_column);
+//Table_Info* table;
+//table=get_table(filename1,needed_column);
+results* one;
+    results* two;
+    one=big_short(filename1,needed_column);
+    printf("\n A \n");
+    two=big_short(filename2,needed_column);
+    printf("\n B \n");
 //printf("\n \n \n");
 
-
+/*
 
 j=0;
 for(i=0;i<table->rows-1;i++){
     if(not_yet->matrix[i][1]<=not_yet->matrix[i+1][1]){
         j++;
-    }else{
-     //   printf("fuuuckkkk  %d \n",i);
     }
-
-
 }
+    printf("j %d rows %d \n",j,table->rows);
+j=0;
+uint64_t a,b,c;
+    for(i=0;i<table->rows-1;i++) {
+        a=not_yet->matrix[i][1];
+        b=table->TableA[not_yet->matrix[i][0]][1];
+        c=table->TableA[1948][1];
+      //  print(table,1945,1955);
+        k=not_yet->matrix[i][0];
+        k=table->TableA[not_yet->matrix[i][0]][0];
 
-printf("j %d rows %d",j,table->rows);
+        if(not_yet->matrix[i][1]==table->TableA[not_yet->matrix[i][0]][needed_column-1]){
+
+                    j++;}
+    }
+printf("j %d ples %d",j,table->rows);
+*/
+//free_table(table);
+
+   // printf("not yet %d ",not_yet->rows);
+
+
+    clock_t start = clock();
+
+Head_join* test;
+test=join(one,two);
 
     clock_t stop = clock();
     double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
-    printf("not yet %d ",not_yet->rows);
+
     printf("\nTime elapsed: %.5f\n", elapsed*10);
+
+
+j=1;
+int max;
+List_join* kl;
+kl=test->first;
+max=test->size;
+while(kl!=NULL){
+    if(j>1)
+        max=120;
+    for(i=0;i<max;i++){
+       printf("::: %d | %d \n ",kl->id_matrix[i][0],kl->id_matrix[i][1]);
+
+    }
+    j++;
+printf("\n next bucket is %d\n",j);
+    kl=kl->next;
+}
+printf(" %d ",test->size);
+
+
 
 
     return 0;
